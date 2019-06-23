deb29b19c7286c2354b8f9ea92700d549d53d59d
Mike Murray <MikeMurrayDev@gmail.com>
2019-06-23 14:39:40 -0700

    Dynamically loaded table with minor Bootstrap

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON("data.json", function(data){
+        var column = [];
+        $.each(data, function(key, value){
+            column.push("<tr><td>" + value.name + "</td><td>" + value.description + 
+                        "</td><td><a href='#'" + " onclick=window.open('https://www.google.com/maps?q=" + value.location + "'" + ")><button>Map</button></a></td></tr>");        
+        });
+        $("table").append(column);
+    });
 });
