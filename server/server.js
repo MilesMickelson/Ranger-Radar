fc6cb096ca4e1d106530d65f21d2cda1f5e5dae8
Mike Murray <MikeMurrayDev@gmail.com>
2019-11-09 12:01:43 -0800

    adds Twilio package and server code for Twilio (WIP)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 const app = express();
+dotenv.config();
 
 app.use(express.static('dist'));
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(urlEncodedParser);
 
 module.exports = app;
